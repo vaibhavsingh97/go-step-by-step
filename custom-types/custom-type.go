276d0cc1dc3e7dd221251fb49475d883b79d44aa
Vaibhav Singh <vaibhav.singh.14cse@bml.edu.in>
2018-11-28 09:56:34 +0530

    learnt about custom types

---
+++ custom-types/custom-type.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+var x int
+
+type apple int
+
+var y apple
+
+func main() {
+	x = 100
+	y = 10001
+
+	fmt.Println(x)
+	fmt.Printf("Type: %T\n", x)
+
+	fmt.Println(y)
+	fmt.Printf("Type: %T\n", y)
+
+	// This will give error as TYPE apple cannot be assigned to TYPE int
+	// for assignments both have to be have same types
+	// x = y --> Error
+
+}
