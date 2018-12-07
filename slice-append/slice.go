d9bdc1484b44d7e353025c1fc9d965c653c9c62a
Vaibhav Singh <vaibhav.singh.14cse@bml.edu.in>
2018-12-07 13:31:18 +0530

    learnt how to appned elements in slice

---
+++ slice-append/slice.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := []int{1, 2, 3, 4, 5}
+	fmt.Println(x)
+	x = append(x, 33, 44, 55, 66)
+	fmt.Println(x)
+	y := []int{99, 88, 77, 66}
+	// if  "..." before some element than it will take unlimited number of elements
+	// if "..." after the element than it will expand all the elements
+	x = append(x, y...)
+	fmt.Println(x)
+}
