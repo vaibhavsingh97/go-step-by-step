0e34426c0fdea03c088108c1f74055657747c222
Vaibhav Singh <vaibhav.singh.14cse@bml.edu.in>
2018-12-10 23:58:39 +0530

    learnt about json mashal in go

---
+++ json-marshal/json.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type person struct {
+	First string
+	Last  string
+	Age   int
+}
+
+func main() {
+	p1 := person{
+		First: "James",
+		Last:  "Bond",
+		Age:   33,
+	}
+	p2 := person{
+		First: "vaibhav",
+		Last:  "Singh",
+		Age:   21,
+	}
+	people := []person{p1, p2}
+	fmt.Println(people)
+
+	bs, err := json.Marshal(people)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(bs))
+}
